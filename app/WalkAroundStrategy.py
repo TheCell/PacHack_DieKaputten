b96525728d49400b05f7f73ea96b07e8f4e0502a
TheCell <simon_hischier@hotmail.com>
2018-11-24 17:20:13 +0100

    almost working

--- app/WalkAroundStrategy.py
+++ app/WalkAroundStrategy.py
@@ -8,6 +8,8 @@ import random
 class WalkAroungStrategy(Strategy):
     preference = ReturnDirections.WEST
     choice = ReturnDirections.WEST
+    previousmove = None
+    preferedDirection = ReturnDirections.WEST
 
     def __init__(self, game_field, my_player, my_position):
         super().__init__(game_field, my_player, my_position)
@@ -19,10 +21,31 @@ class WalkAroungStrategy(Strategy):
             moves.append(move)
 
         if WalkAroungStrategy.preference in moves:
+            if (WalkAroungStrategy.previousmove is not ReturnDirections.WEST) and (ReturnDirections.EAST in moves):
+                WalkAroungStrategy.preference = ReturnDirections.EAST
+
             WalkAroungStrategy.choice = WalkAroungStrategy.preference
             print("is in if " + str(WalkAroungStrategy.choice))
         else:
-            WalkAroungStrategy.preference = random.choice(moves)
+            if WalkAroungStrategy.previousmove is not None:
+                if len(moves) > 1:
+                    if WalkAroungStrategy.previousmove == ReturnDirections.NORTH:
+                        moves.remove(ReturnDirections.SOUTH)
+                    elif WalkAroungStrategy.previousmove == ReturnDirections.SOUTH:
+                        moves.remove(ReturnDirections.NORTH)
+                    elif WalkAroungStrategy.previousmove == ReturnDirections.EAST:
+                        moves.remove(ReturnDirections.WEST)
+                    elif WalkAroungStrategy.previousmove == ReturnDirections.WEST:
+                        moves.remove(ReturnDirections.EAST)
+                else:
+                    print("moves length is not more then 1")
+            if WalkAroungStrategy.preferedDirection in moves:
+                WalkAroungStrategy.preference = WalkAroungStrategy.preferedDirection
+            else:
+                WalkAroungStrategy.preference = random.choice(moves)
+
             WalkAroungStrategy.choice = WalkAroungStrategy.preference
             print("is in else " + str(WalkAroungStrategy.choice))
+
+        WalkAroungStrategy.previousmove = WalkAroungStrategy.choice
         return WalkAroungStrategy.choice
