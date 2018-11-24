3b467ffe89d7853ed89a460d1d7269ff74c70827
TheCell <simon_hischier@hotmail.com>
2018-11-24 16:55:15 +0100

    walkingAround working

--- app/WalkAroundStrategy.py
+++ app/WalkAroundStrategy.py
@@ -1,23 +1,28 @@
 from app.dto.ReturnDirections import ReturnDirections
 from app.strategy import Strategy
 from app.StrategyHelper import StrategyHelper
+from app.Bot import Bot
+
 import random
 
 class WalkAroungStrategy(Strategy):
+    preference = ReturnDirections.WEST
+    choice = ReturnDirections.WEST
+
     def __init__(self, game_field, my_player, my_position):
         super().__init__(game_field, my_player, my_position)
-        if self.my_player == 1:
-            self.preference = ReturnDirections.LEFT
-        else:
-            self.preference = ReturnDirections.RIGHT
 
     def get_move(self):
+        print(WalkAroungStrategy.preference)
         moves = []
         for move in StrategyHelper.get_legal_directions(self.my_position, self.game_field):
             moves.append(move)
-        if self.preference in moves:
-            self.choice = self.preference
+
+        if WalkAroungStrategy.preference in moves:
+            WalkAroungStrategy.choice = WalkAroungStrategy.preference
+            print("is in if " + str(WalkAroungStrategy.choice))
         else:
-            self.preference = random.choice(moves)
-            self.choice = self.preference
-        return self.choice
+            WalkAroungStrategy.preference = random.choice(moves)
+            WalkAroungStrategy.choice = WalkAroungStrategy.preference
+            print("is in else " + str(WalkAroungStrategy.choice))
+        return WalkAroungStrategy.choice
