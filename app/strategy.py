39b7b9ddd15b19fbbb78a9c895365733c5ee6615
iasalzma <joel.salzmann@stud.hslu.ch>
2018-11-24 14:44:22 +0100

    tried implementing a strategy

--- app/strategy.py
+++ app/strategy.py
@@ -6,7 +6,7 @@ class Strategy:
         self.my_player = my_player
         self.my_position = my_position
         self.optimum_position = False
-        self.choice = ReturnDirections.random()
+        self.choice = ReturnDirections.STOP
 
     def get_move(self):
         return self.choice
